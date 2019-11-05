78981a15a2bccc51e2bd11bd3d8f660d7088b84c
Vasilii Sitdikov <v.k.sitdikov@gmail.com>
2019-11-05 17:37:20 +0100

    Lesson 8 attempt 2

--- les_8/les_8_task_3.py
+++ les_8/les_8_task_3.py
@@ -10,3 +10,33 @@
 
 
 # Solution:
+class MyEx(Exception):
+    def __init__(self, txt, lst):
+        super().__init__(txt)
+        self.input = lst
+        self.txt = txt
+
+
+def checkargs(lst):
+    for el in lst:
+        print(el)
+        if type(el) == bool:
+            raise MyEx(f'There is some "bool" value "{el}"', lst)
+        elif type(el) == str:
+            raise MyEx(f'There is some "str" value {el}', lst)
+
+
+def trier(lst):
+
+    try:
+        checkargs(lst)
+    except MyEx as err:
+        print(err.txt)
+
+
+# Пока реализовано так. Не очень понятно, что имелось ввиду в задании.
+# Получая значения от пользователя через input мы всегда будем иметь строковые данные.
+my_list = [1, 2.0, True, 'text']
+trier(my_list)
+my_list = [1, 2.0, 'text', True]
+trier(my_list)
